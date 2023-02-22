bf2d7fd1e22dd92b1aae3e89538cf2caae57402c
Pablo Reyes <chugo940@gmail.com>
2023-02-22 14:34:09 -0600

    cambios 4

--- api/src/app.js
+++ api/src/app.js
@@ -14,11 +14,13 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-
-// origin     "https://pi-dogs-main-gold.vercel.app",
+// headers =     "https://pi-dogs-main-gold.vercel.app"
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000" || "https://pi-dogs-main-gold.vercel.app"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
